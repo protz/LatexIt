268f80017349065330abe3f2b8dc383e2f348a75
jonathan@nala.xulforum.org <none@none>
2009-10-28 14:44:06 +0100

    Works up to a certain point (cannot send the email).

--- content/main.js
+++ content/main.js
@@ -10,6 +10,13 @@ var tblatex_on_toolbarbutton_clicked = function () {};
     else
       node2.parentNode.appendChild(node1);
   }
+  
+  function get_cwd() {
+    return Components.classes["@mozilla.org/file/directory_service;1"].
+      getService(Components.interfaces.nsIProperties).
+      get("CurWorkD", Components.interfaces.nsIFile).path;
+  }
+
 
   /* splits #text [ some text $\LaTeX$ some text ] into three separates text
    * nodes and returns the list of latex nodes */
@@ -37,8 +44,85 @@ var tblatex_on_toolbarbutton_clicked = function () {};
     return latex_nodes;
   }
 
-  function run_latex() {
-    return [true, "http://www.google.com/intl/en_ALL/images/logo.gif"];
+  function run_latex(latex_expr) {
+    dump("Generating LaTeX expression "+latex_expr+"\n");
+    let latex_bin = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+    latex_bin.initWithPath("/usr/bin/latex");
+    let dvips_bin = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+    dvips_bin.initWithPath("/usr/bin/dvips");
+    let convert_bin = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+    convert_bin.initWithPath("/usr/bin/convert");
+
+    let temp_dir = Components.classes["@mozilla.org/file/directory_service;1"].
+      getService(Components.interfaces.nsIProperties).
+      get("TmpD", Components.interfaces.nsIFile).path;
+    dump("Using "+temp_dir+" as the temporary directory\n");
+    let temp_file = Components.classes["@mozilla.org/file/directory_service;1"].
+      getService(Components.interfaces.nsIProperties).
+      get("TmpD", Components.interfaces.nsIFile);
+    temp_file.append("tblatex.tex");
+    temp_file.createUnique(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0666);
+    dump("Using "+temp_file.path+" as the temporary file\n");
+    let cwd = get_cwd();
+    dump("Current directory is "+cwd+"\n");
+    let temp_file_noext = temp_file.leafName.substr(0, temp_file.leafName.lastIndexOf("."));
+
+    let data =
+      "\\documentclass{article}\n"+
+      "\\pagestyle{empty}\n"+
+      "\\begin{document}\n"+
+      "\\begin{center}\n"+
+      latex_expr+
+      "\\end{center}\n"+
+      "\\end{document}\n";
+
+    // file is nsIFile, data is a string
+    let foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].
+      createInstance(Components.interfaces.nsIFileOutputStream);
+
+    // use 0x02 | 0x10 to open file for appending.
+    foStream.init(temp_file, 0x02 | 0x08 | 0x20, 0666, 0); 
+    // write, create, truncate
+    // In a c file operation, we have no need to set file mode with or operation,
+    // directly using "r" or "w" usually.
+
+    // if you are sure there will never ever be any non-ascii text in data you can 
+    // also call foStream.writeData directly
+    let converter = Components.classes["@mozilla.org/intl/converter-output-stream;1"].
+      createInstance(Components.interfaces.nsIConverterOutputStream);
+    converter.init(foStream, "UTF-8", 0, 0);
+    converter.writeString(data);
+    converter.close(); // this closes foStream
+
+
+    let latex_process = Components.classes["@mozilla.org/process/util;1"]
+      .createInstance(Components.interfaces.nsIProcess);
+    latex_process.init(latex_bin);
+    latex_process.run(true, [temp_file.path], 1);
+
+    let dvi_file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+    dvi_file.initWithPath(cwd);
+    dvi_file.append(temp_file_noext+".dvi");
+
+    let dvips_process = Components.classes["@mozilla.org/process/util;1"]
+      .createInstance(Components.interfaces.nsIProcess);
+    dvips_process.init(dvips_bin);
+    dvips_process.run(true, [dvi_file.path], 1);
+
+    let ps_file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+    ps_file.initWithPath(cwd);
+    ps_file.append(temp_file_noext+".ps");
+
+    let png_file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+    png_file.initWithPath(cwd);
+    png_file.append(temp_file_noext+".png");
+
+    let convert_process = Components.classes["@mozilla.org/process/util;1"]
+      .createInstance(Components.interfaces.nsIProcess);
+    convert_process.init(convert_bin);
+    convert_process.run(true, [dvi_file.path, "-trim", png_file.path], 3);
+
+    return [true, "file://"+png_file.path];
   }
 
   /* replaces each latex text node with the corresponding generated image */
@@ -46,8 +130,7 @@ var tblatex_on_toolbarbutton_clicked = function () {};
     let editor = GetCurrentEditor();
     for (let i = 0; i < nodes.length; ++i) {
       let elt = nodes[i];
-      dump(elt.textContent+"\n");
-      let [st, url] = run_latex(elt.nodeData);
+      let [st, url] = run_latex(elt.nodeValue);
       if (st) {
         let img = editor.createElementWithDefaults("img");
         img.setAttribute("src", url);
