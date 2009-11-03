216192a7a34261e2f8450cf51a78a9c5fc062cda
jonathan@nala.xulforum.org <none@none>
2009-11-03 11:16:39 +0100

    Thunderbird 2.0 compatibility, added dialogs, undo, some other extra features.

--- content/main.js
+++ content/main.js
@@ -1,10 +1,20 @@
-var tblatex_on_toolbarbutton_clicked = function () {};
-
 (function () {
   if (document.location.href != "chrome://messenger/content/messengercompose/messengercompose.xul")
     return;
 
-  let prefs = Components.classes["@mozilla.org/preferences-service;1"]
+  var g_undo_func = null;
+  var g_image_cache = {};
+
+  function push_undo_func(f) {
+    var old_undo_func = g_undo_func;
+    var g = function () {
+      g_undo_func = old_undo_func;
+      f();
+    };
+    g_undo_func = g;
+  }
+
+  var prefs = Components.classes["@mozilla.org/preferences-service;1"]
     .getService(Components.interfaces.nsIPrefService)
     .getBranch("tblatex.");
 
@@ -18,24 +28,24 @@ var tblatex_on_toolbarbutton_clicked = function () {};
   /* splits #text [ some text $\LaTeX$ some text ] into three separates text
    * nodes and returns the list of latex nodes */
   function split_text_nodes(node) {
-    let latex_nodes = [];
+    var latex_nodes = [];
     if (node.nodeType == node.TEXT_NODE) {
-      let re = /\$\$[^\$]+\$\$|\$[^\$]+\$/g;
-      let matches = node.nodeValue.match(re);
+      var re = /\$\$[^\$]+\$\$|\$[^\$]+\$/g;
+      var matches = node.nodeValue.match(re);
       if (matches) {
-        for (let i = matches.length - 1; i >= 0; --i) {
-          let match = matches[i];
-          let j = node.nodeValue.lastIndexOf(match);
-          let k = j + match.length;
+        for (var i = matches.length - 1; i >= 0; --i) {
+          var match = matches[i];
+          var j = node.nodeValue.lastIndexOf(match);
+          var k = j + match.length;
           insertAfter(document.createTextNode(node.nodeValue.substr(k, (node.nodeValue.length-k))), node);
-          let latex_node = document.createTextNode(match);
+          var latex_node = document.createTextNode(match);
           latex_nodes.push(latex_node);
           insertAfter(latex_node, node);
           node.nodeValue = node.nodeValue.substr(0, j);
         }
       }
     } else if (node.childNodes && node.childNodes.length) {
-      for (let i = node.childNodes.length - 1; i >= 0; --i)
+      for (var i = node.childNodes.length - 1; i >= 0; --i)
         latex_nodes = latex_nodes.concat(split_text_nodes(node.childNodes[i]));
     }
     return latex_nodes;
@@ -44,57 +54,58 @@ var tblatex_on_toolbarbutton_clicked = function () {};
   /* This *has* to be global. If image a.png is inserted, then modified, then
    * inserted again in the same mail, the OLD a.png is displayed because of some
    * cache which I haven't found a way to invalidate yet. */
-  let g_suffix = 1;
+  var g_suffix = 1;
 
   function run_latex(latex_expr) {
-    let log = "*** Found expression "+latex_expr+"\n";
-    let init_file = function(path) {
-      let f = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+    var log = "";
+    if (g_image_cache[latex_expr])
+      return [true, "file://"+g_image_cache[latex_expr], log+"Image was already generated\n"];
+
+    var init_file = function(path) {
+      var f = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
       f.initWithPath(path);
       return f;
     }
-    let init_process = function(path) {
-      let process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
+    var init_process = function(path) {
+      var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
       process.init(path);
       return process;
     }
 
     dump("Generating LaTeX expression "+latex_expr+"\n");
-    let latex_bin = init_file(prefs.getCharPref("latex_path"));
+    var latex_bin = init_file(prefs.getCharPref("latex_path"));
     if (!latex_bin.exists()) {
       log += "Wrong path for latex bin. Please set the right path in the options dialog first.\n";
       return [false, "", log];
     }
-    let dvips_bin = init_file(prefs.getCharPref("dvips_path"));
+    var dvips_bin = init_file(prefs.getCharPref("dvips_path"));
     if (!dvips_bin.exists()) {
       log += "Wrong path for dvips bin. Please set the right path in the options dialog first.\n";
       return [false, "", log];
     }
-    let convert_bin = init_file(prefs.getCharPref("convert_path"));
+    var convert_bin = init_file(prefs.getCharPref("convert_path"));
     if (!convert_bin.exists()) {
       log += "Wrong path for convert bin. Please set the right path in the options dialog first.\n";
       return [false, "", log];
     }
 
-    let temp_dir = Components.classes["@mozilla.org/file/directory_service;1"].
+    var temp_dir = Components.classes["@mozilla.org/file/directory_service;1"].
       getService(Components.interfaces.nsIProperties).
       get("TmpD", Components.interfaces.nsIFile).path;
-    let temp_file = init_file(temp_dir);
+    var temp_file = init_file(temp_dir);
     temp_file.append("tblatex-"+g_suffix+".png");
     while (temp_file.exists()) {
       g_suffix++;
       temp_file = init_file(temp_dir);
       temp_file.append("tblatex-"+g_suffix+".png");
     }
-    let temp_file_noext = "tblatex-"+g_suffix;
+    var temp_file_noext = "tblatex-"+g_suffix;
     temp_file = init_file(temp_dir);
     temp_file.append("tblatex-"+g_suffix+".tex");
     if (temp_file.exists()) temp_file.remove(false);
 
-    let data = prefs.getCharPref("template").replace("__REPLACEME__", latex_expr);
-
     // file is nsIFile, data is a string
-    let foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].
+    var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].
       createInstance(Components.interfaces.nsIFileOutputStream);
 
     // use 0x02 | 0x10 to open file for appending.
@@ -105,31 +116,31 @@ var tblatex_on_toolbarbutton_clicked = function () {};
 
     // if you are sure there will never ever be any non-ascii text in data you can 
     // also call foStream.writeData directly
-    let converter = Components.classes["@mozilla.org/intl/converter-output-stream;1"].
+    var converter = Components.classes["@mozilla.org/intl/converter-output-stream;1"].
       createInstance(Components.interfaces.nsIConverterOutputStream);
     converter.init(foStream, "UTF-8", 0, 0);
-    converter.writeString(data);
+    converter.writeString(latex_expr);
     converter.close(); // this closes foStream
 
 
-    let latex_process = init_process(latex_bin);
+    var latex_process = init_process(latex_bin);
     latex_process.run(true, ["-output-directory="+temp_dir, "-interaction=batchmode", temp_file.path], 3);
     temp_file.remove(false);
     if (latex_process.exitValue) {
       log += "LaTeX process returned "+latex_process.exitValue+"\nProceeding anyway...\n";
     }
 
-    let dvi_file = init_file(temp_dir);
+    var dvi_file = init_file(temp_dir);
     dvi_file.append(temp_file_noext+".dvi");
     if (!dvi_file.exists()) {
       log += "LaTeX did not output a .dvi file, something definitely went wrong. Aborting.\n";
       return [false, "", log];
     }
 
-    let ps_file = init_file(temp_dir);
+    var ps_file = init_file(temp_dir);
     ps_file.append(temp_file_noext+".ps");
 
-    let dvips_process = init_process(dvips_bin);
+    var dvips_process = init_process(dvips_bin);
     dvips_process.run(true, ["-o", ps_file.path, "-E", dvi_file.path], 4);
     dvi_file.remove(false);
     if (dvips_process.exitValue) {
@@ -137,37 +148,37 @@ var tblatex_on_toolbarbutton_clicked = function () {};
       return [false, "", log];
     }
 
-    let png_file = init_file(temp_dir);
+    var png_file = init_file(temp_dir);
     png_file.append(temp_file_noext+".png");
 
-    let convert_process = init_process(convert_bin);
-    let dpi = prefs.getIntPref("dpi");
+    var convert_process = init_process(convert_bin);
+    var dpi = prefs.getIntPref("dpi");
     convert_process.run(true, ["-units", "PixelsPerInch", "-density", dpi, ps_file.path, "-trim", png_file.path], 7);
     ps_file.remove(false);
     if (convert_process.exitValue) {
       log += "convert failed with error code "+convert_process.exitValue+". Aborting.\n";
       return [false, "", log];
     }
+    g_image_cache[latex_expr] = png_file.path;
 
     ["log", "aux"].forEach(function (ext) {
-        let file = init_file(temp_dir);
+        var file = init_file(temp_dir);
         file.append(temp_file_noext+"."+ext);
         file.remove(false);
       });
 
-    Application.console.log(png_file.path);
     return [true, "file://"+png_file.path, log];
   }
 
   function open_log() {
-    let want_log = prefs.getBoolPref("log");
+    var want_log = prefs.getBoolPref("log");
     if (!want_log)
       return (function () {});
 
-    let editor = document.getElementById("content-frame");
-    let edocument = editor.contentDocument;
-    let body = edocument.querySelector("body");
-    let div = edocument.createElement("div");
+    var editor = document.getElementById("content-frame");
+    var edocument = editor.contentDocument;
+    var body = edocument.getElementsByTagName("body")[0];
+    var div = edocument.createElement("div");
     if (body.firstChild)
       body.insertBefore(div, body.firstChild);
     else
@@ -181,46 +192,54 @@ var tblatex_on_toolbarbutton_clicked = function () {};
         "color: black !important;\">X</a>"+
       "<span style=\"font-family: sans-serif; font-weight: bold; font-size: large\">"+
       "TBLaTeX run report...</span><br />";
-    let a = div.querySelector("a");
+    var a = div.getElementsByTagName("a")[0];
     a.addEventListener('click', {
         handleEvent: function (event) {
           a.parentNode.parentNode.removeChild(a.parentNode);
           return false
         }
       }, false);
-    let p = edocument.createElement("pre");
+    var p = edocument.createElement("pre");
     p.setAttribute("style", "max-height: 500px; overflow: auto;");
     div.appendChild(p);
-    let f = function (text) { let n = edocument.createTextNode(text); p.appendChild(n); };
+    var f = function (text) { var n = edocument.createTextNode(text); p.appendChild(n); };
     return f;
   }
 
   function close_log() {
-    let editor = document.getElementById("content-frame");
-    let edocument = editor.contentDocument;
-    let div = edocument.getElementById("tblatex-log");
+    var editor = document.getElementById("content-frame");
+    var edocument = editor.contentDocument;
+    var div = edocument.getElementById("tblatex-log");
     if (div)
       div.parentNode.removeChild(div);
   }
 
   /* replaces each latex text node with the corresponding generated image */
   function replace_latex_nodes(nodes) {
-    let write_log = open_log();
-    let editor = GetCurrentEditor();
+    var template = prefs.getCharPref("template");
+    var write_log = open_log();
+    var editor = GetCurrentEditor();
     if (!nodes.length)
       write_log("No LaTeX $$ expressions found\n");
-    for (let i = 0; i < nodes.length; ++i) {
-      let elt = nodes[i];
-      let [st, url, log] = run_latex(elt.nodeValue);
-      Application.console.log(url);
+    for (var i = 0; i < nodes.length; ++i) {
+      var elt = nodes[i];
+      write_log("*** Found expression "+elt.nodeValue+"\n");
+      var latex_expr = template.replace("__REPLACEME__", elt.nodeValue);
+      var [st, url, log] = run_latex(latex_expr);
       write_log(log);
       if (st) {
-        let img = editor.createElementWithDefaults("img");
+        var img = editor.createElementWithDefaults("img");
         img.setAttribute("alt", elt.nodeValue);
         img.setAttribute("src", url);
         img.setAttribute("style", "vertical-align: middle");
         elt.parentNode.insertBefore(img, elt);
         elt.parentNode.removeChild(elt);
+        push_undo_func(function () {
+          editor.beginTransaction();
+          img.parentNode.insertBefore(elt, img);
+          img.parentNode.removeChild(img);
+          editor.endTransaction();
+        });
       }
     }
   }
@@ -228,18 +247,18 @@ var tblatex_on_toolbarbutton_clicked = function () {};
   function on_toolbarbutton_clicked(event) {
     /* safety checks */
     if (event.button == 2) return;
-    let editor_elt = document.getElementById("content-frame");
+    var editor_elt = document.getElementById("content-frame");
     if (editor_elt.editortype != "htmlmail") {
       alert("Cannot Latexify plain text emails. Use Options > Format to switch to HTML Mail.");
       return;
     }
 
-    let editor = GetCurrentEditor();
+    var editor = GetCurrentEditor();
     editor.beginTransaction();
     try {
       close_log();
-      let body = editor_elt.contentDocument.querySelector("body");
-      let latex_nodes = split_text_nodes(body);
+      var body = editor_elt.contentDocument.getElementsByTagName("body")[0];
+      var latex_nodes = split_text_nodes(body);
       replace_latex_nodes(latex_nodes);
     } catch (e /*if false*/) { /*XXX do not catch errors to get full backtraces in dev cycles */
       Application.console.log("TBLatex error: "+e);
@@ -247,5 +266,68 @@ var tblatex_on_toolbarbutton_clicked = function () {};
     editor.endTransaction();
   }
 
+  function undo(event) {
+    try {
+      if (g_undo_func)
+        g_undo_func();
+    } catch (e) {
+      Application.console.log("TBLatex Error (while undoing) "+e);
+    }
+    event.stopPropagation();
+  }
+
+  function undo_all(event) {
+    try {
+      while (g_undo_func)
+        g_undo_func();
+    } catch (e) {
+      Application.console.log("TBLatex Error (while undoing) "+e);
+    }
+    event.stopPropagation();
+  }
+
+  var g_complex_input = null;
+
+  function open_insert_dialog(event) {
+    var editor = GetCurrentEditor();
+    var f = function (latex_expr) {
+      g_complex_input = latex_expr;
+      editor.beginTransaction();
+      try {
+        close_log();
+        var write_log = open_log();
+        var [st, url, log] = run_latex(latex_expr);
+        log = log || "Everything went OK.\n";
+        write_log(log);
+        if (st) {
+          var img = editor.createElementWithDefaults("img");
+          img.setAttribute("alt", latex_expr);
+          img.setAttribute("src", url);
+          img.setAttribute("style", "vertical-align: middle");
+          editor.insertElementAtSelection(img, true);
+          push_undo_func(function () {
+            editor.beginTransaction();
+            img.parentNode.removeChild(img);
+            close_log();
+            editor.endTransaction();
+          });
+        }
+      } catch (e) {
+        Application.console.log("TBLatex Error (while inserting) "+e);
+      }
+      editor.endTransaction();
+    }
+    var template = g_complex_input || prefs.getCharPref("template");
+    window.openDialog("chrome://tblatex/content/insert.xul", "", "chrome", f, template);
+    event.stopPropagation();
+  }
+
   tblatex_on_toolbarbutton_clicked = on_toolbarbutton_clicked;
+  window.addEventListener("load",
+    function () {
+      document.getElementById("tblatex-button-1").addEventListener("command", on_toolbarbutton_clicked, false);
+      document.getElementById("tblatex-button-undo").addEventListener("command", undo, false);
+      document.getElementById("tblatex-button-undo_all").addEventListener("command", undo_all, false);
+      document.getElementById("tblatex-button-insert_complex").addEventListener("command", open_insert_dialog, false);
+    }, false);
 })()
