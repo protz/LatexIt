82b48360e4775742be63b9d3506ac166c9f855ed
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2010-10-18 14:26:27 +0200

    Fix annoying focus bug on Windows.

---
+++ content/osx-path-hacks.js
@@ -0,0 +1,19 @@
+var isOSX = ("nsILocalFileMac" in Components.interfaces);
+var Cc = Components.classes;
+var Ci = Components.interfaces;
+
+if (isOSX) {
+  var env = Cc["@mozilla.org/process/environment;1"].createInstance(Ci.nsIEnvironment);
+  var paths = env.get("PATH").split(":");
+  var suggestions = [
+    "/usr/local/bin",
+    "/usr/texbin",
+    "/usr/X11/bin"
+  ];
+  for (var i = 0; i < suggestions.length; ++i) {
+    if (paths.indexOf(suggestions[i]) < 0)
+      paths.push(suggestions[i]);
+  }
+  env.set("PATH", paths.join(":"));
+  dump("New path: "+env.get("PATH")+"\n");
+}
