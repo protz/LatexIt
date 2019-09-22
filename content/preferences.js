d850f573121ef1a3227ef3469a40ae9cf688eb0b
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2019-09-22 19:36:53 -0400

    Fix entering preferences manually

--- content/preferences.js
+++ content/preferences.js
@@ -1,7 +1,6 @@
 Preferences.addAll([
   { id: "tblatex.latex_path", type: "string" },
-  { id: "tblatex.dvips_path", type: "string" },
-  { id: "tblatex.convert_path", type: "string" },
+  { id: "tblatex.dvipng_path", type: "string" },
   { id: "tblatex.dpi", type: "int" },
   { id: "tblatex.log", type: "bool" },
   { id: "tblatex.debug", type: "bool" },
