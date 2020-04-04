eba432cc69c28cb20793a1188c632c51d4e3328b
Stephan Helma <s.p.helma@gmx.net>
2020-04-04 02:05:11 +0200

    Select the insertion marker when opening the insert complex LaTeX dialog. Cleaned up the options dialog.

--- content/insert.js
+++ content/insert.js
@@ -4,7 +4,13 @@ function on_ok() {
 }
 
 window.addEventListener("load", function (event) {
-  document.getElementById("tblatex-expr").value = window.arguments[1];
+  var marker = "__REPLACEME__";
+  var textarea = document.getElementById("tblatex-expr");
+  textarea.value = window.arguments[1];
+  var start = textarea.value.indexOf(marker);
+  textarea.focus();
+  if (start > -1)
+    textarea.setSelectionRange(start, start+marker.length);
 }, false);
 
 var prefs = Components.classes["@mozilla.org/preferences-service;1"]
