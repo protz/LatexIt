d974610e6b8f55fbae9c5d01be24a2904fe07de5
Stephan Helma <s.p.helma@gmx.net>
2020-04-05 00:40:47 +0200

    - Increased version to 0.7.1. - Fixed the insertion of complex LaTeX (Bug #43). - Select the insertion marker when opening the insert complex LaTeX dialog. - Cleaned up the options dialog. - Replaced place holder __REPLACEME__ with __REPLACE_ME__, which IMHO is easier   to the eye. - Added alignment of inserted pictures to the text baseline (Bug #36). - Added improvements of LaTeXIt! button:   - Button is disabled for plain text composer windows (Bug #48).   - Middle click calls 'undo' (Bug #2).   - Shift+middle click calls 'undo_all'. - Added context menu to composer window (Bug #49). - Added LaTeXIt! to the Add-ons menu to access the preferences (Bug #42). - Removed the configuration item from toolbar and context menues, because they   are not needed anymore. - Delete all cached images when the composer window is closed (Bug #44). - Added autoscaling of images to font size (can be switched off in the   preferences, where a size can be manually set). - When inserting a complex LaTeX, the size can either be automatically   calculated from the surrounding text or set manually. - Use 'font size' (in px) instead of 'resolution'.

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,8 +1,9 @@
 pref("tblatex.latex_path", "");
 pref("tblatex.dvipng_path", "");
-pref("tblatex.dpi", 150);
+pref("tblatex.autodpi", true);
+pref("tblatex.font_px", 16);
 pref("tblatex.log", true);
 pref("tblatex.debug", false);
-pref("tblatex.template", "\\documentclass{article}\n\\usepackage[utf8x]{inputenc}\n\\pagestyle{empty}\n\\begin{document}\n__REPLACEME__ %this is where your LaTeX expression goes\n\\end{document}\n");
+pref("tblatex.template", "\\documentclass{article}\n\\usepackage[utf8x]{inputenc}\n\\usepackage[active,displaymath,textmath]{preview}\n\\pagestyle{empty}\n\\begin{document}\n__REPLACE_ME__ %this is where your LaTeX expression goes\n\\end{document}\n");
 pref("tblatex.firstrun", 0);
 
