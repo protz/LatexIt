869048b4acb8a8baa0bdfbfe0e148439165b83ed
Stephan <s.p.helma@gmx.net>
2020-10-01 14:10:28 +0200

    - Fixed baseline alignment on multiple runs (Bug #64).

--- content/main.js
+++ content/main.js
@@ -104,8 +104,8 @@ var tblatex = {
 
       if (g_image_cache[latex_expr+font_px]) {
         if (debug)
-          log += "Found a cached image file "+g_image_cache[latex_expr+font_px]+", returning\n";
-        return [0, g_image_cache[latex_expr+font_px], 0, log+"Image was already generated\n"];
+          log += "Found a cached image file "+g_image_cache[latex_expr+font_px]["png"]+" (depth="+g_image_cache[latex_expr+font_px]["depth"]+"), returning\n";
+        return [0, g_image_cache[latex_expr+font_px]["png"], g_image_cache[latex_expr+font_px]["depth"], log+"Image was already generated\n"];
       }
 
       var init_file = function(path) {
@@ -282,7 +282,6 @@ var tblatex = {
         log += "!!! dvipng failed with error code "+shell_process.exitValue+". Aborting.\n";
         return [2, "", 0, log];
       }
-      g_image_cache[latex_expr+font_px] = png_file.path;
 
       if (debug) {
         log += ("*** Status is "+st+"\n");
@@ -300,6 +299,7 @@ var tblatex = {
       // Read the depth (distance between base of image and baseline) from the depth file
       if (!depth_file.exists()) {
         log += "dvipng did not put out a depth file. Continuing without alignment.\n";
+        g_image_cache[latex_expr+font_px] = {"png": png_file.path, "depth": 0};
         return [st, png_file.path, 0, log];
       }
 
@@ -336,6 +336,7 @@ var tblatex = {
       //  in case of error.
       temp_file.remove(false);
 
+      g_image_cache[latex_expr+font_px] = {"png": png_file.path, "depth": depth};
       return [st, png_file.path, depth, log];
     } catch (e) {
       dump(e+"\n");
@@ -689,7 +690,7 @@ var tblatex = {
       for (var key in g_image_cache) {
         var f = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsIFile);
         try {
-          f.initWithPath(g_image_cache[key]);
+          f.initWithPath(g_image_cache[key]["png"]);
           f.remove(false);
           delete g_image_cache[key];
         } catch (e) { }
